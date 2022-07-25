54c321bc91deb64ff6f9b33863fda8a459cb79c8
Antosha Baboshin <antosha.js@gmail.com>
2022-07-26 00:14:25 +0300

    feat: start functions-n-closures-tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -157,7 +157,7 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
 function partialUsingArguments(fn, ...args1) {
-  return (...args) => fn(...args, ...args1);
+  return (...args) => fn(...args1, ...args);
 }
 
 
@@ -178,8 +178,8 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function* getIdGeneratorFunction(startFrom) {
+  yield () => startFrom;
 }
 
 
